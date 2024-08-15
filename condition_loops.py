d5e0d1662f5cbc5f016372ebdf83d3aa180ba5e5
Peuge Benjamin <support@atomicdsolutions.com>
2024-08-15 13:38:29 -0400

    Lesson 2 conditions and Loops

---
+++ condition_loops.py
@@ -0,0 +1,20 @@
+temprature = 75
+
+if temprature < 70 
+	print("It's Cold")
+if temprature >= 70 and temprature < 80
+	print("It's Warm")
+else:
+	print("It's Hot")
+
+my_list = [1,2,3,4,5,6]
+
+for ml in my_list:
+	print(ml * ml)
+
+count = 10
+
+while count > 0:
+	print(count)
+	count -= 1
+
